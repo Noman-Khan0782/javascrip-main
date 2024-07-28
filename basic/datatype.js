ca6bfa6ce3d92b46fcab861126e9ad666f918421
Malik Noman khan <120743420+Noman-Khan0782@users.noreply.github.com>
2024-07-28 14:44:17 +0500

    add data type

---
+++ basic/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all javascript code newer version
+// let a= alert(1+2);//we use nodejs not browser
+// console.log(a);
+
+console.log("noman");
+console.log("khan");
+
+console.log(1 + 
+    3); //code readability should be high
+
+    // datatype
+    // number => 2 to power 53 
+    //bigInt
+    // string =>"",''
+    //bolean => true/false
+// NULL => standalone value //mean empty
+// Undefine => 
+    // Symbol=> uniqueness jistra hum react copnonent ko
+// unique karty hai
+
+//object
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "Noman");
